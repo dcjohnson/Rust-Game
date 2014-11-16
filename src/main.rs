5655dbe4be08473e2bf1f63e1ce762fcbdad403c
Dylan Johnson <dylanatasmsa@gmail.com>
2014-11-16 14:14:34 -0600

    Using Cargo to my advantage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+extern crate little_rust_tcp;
+use little_rust_tcp::tcpserver;
+use std::string::String;
+use little_rust_tcp::tcpserver::ServerFunction;
+
+fn main()
+{
+    let server = tcpserver::ServerFunction::new(String::from_str("test"), String::from_str("Hello"));
+    println!("{} {}", server.get_port(), server.get_ip());
+    server.format_ip();
 }
