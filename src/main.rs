c0db4ae29ef048e979560e34fb1910eea7ac6de9
Dylan Johnson <dylanatasmsa@gmail.com>
2014-11-16 14:26:04 -0600

    Removed unneeded code.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 extern crate little_rust_tcp;
-use little_rust_tcp::tcpserver;
 use std::string::String;
 use little_rust_tcp::tcpserver::ServerFunction;
 
 fn main()
 {
-    let server = tcpserver::ServerFunction::new(String::from_str("test"), String::from_str("Hello"));
-    println!("{} {}", server.get_port(), server.get_ip());
-    server.format_ip();
+    let server = ServerFunction::new(String::from_str("test"), String::from_str("Hello"));
+    server.start_server();
 }
