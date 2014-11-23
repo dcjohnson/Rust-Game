d7b601a923fadb6b34e18856801e18c35d463585
Dylan Johnson <dylanatasmsa@gmail.com>
2014-11-23 11:49:12 -0600

    Added test for the server library

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
 extern crate little_rust_tcp;
 use std::string::String;
 use little_rust_tcp::tcpserver::ServerFunction;
+use little_rust_tcp::data::Data;
+use std::io::TcpStream;
 
 fn main()
 {
-    let server = ServerFunction::new(String::from_str("test"), String::from_str("Hello"));
-    server.start_server();
+    let x = DataStruct{x: String::from_str("Hello")};
+    let server = ServerFunction::new(String::from_str("8000"));
+    server.start_server(x);
+}
+
+struct DataStruct
+{
+    x: String
+}
+
+impl Data for DataStruct
+{
+    fn process_request_data(&self, mut request: TcpStream)
+    {
+        request.write(b"Hello World!!\r\n").unwrap();
+    }
 }
